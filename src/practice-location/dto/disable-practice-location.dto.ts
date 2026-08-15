fbc0c0b394fd01ae5cba409ab638dd03a87e3709
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-15 22:50:19 +0800

    m3s5 add practice location disable dto

---
+++ src/practice-location/dto/disable-practice-location.dto.ts
@@ -0,0 +1,15 @@
+import { Equals, IsBoolean, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class DisablePracticeLocationDto {
+  @IsUUID()
+  @IsNotEmpty()
+  practiceLocationId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password!: string;
+
+  @IsBoolean()
+  @Equals(true)
+  confirmDisable!: boolean;
+}
