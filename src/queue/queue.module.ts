f1996881b4ffd7b43dccef6970d88b3c18939238
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-15 22:37:11 +0800

    m3s4 wire substitute secretary governance

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { NotificationModule } from '../notification/notification.module';
 import { ClinicDayCancellationService } from './clinic-day-cancellation.service';
+import { SubstituteSecretaryController } from './substitute-secretary.controller';
+import { SubstituteSecretaryService } from './substitute-secretary.service';
 
 @Module({
-  imports: [NotificationModule],
-  providers: [ClinicDayCancellationService],
-  exports: [ClinicDayCancellationService],
+  imports: [AuthModule, NotificationModule],
+  providers: [ClinicDayCancellationService, SubstituteSecretaryService],
+  controllers: [SubstituteSecretaryController],
+  exports: [ClinicDayCancellationService, SubstituteSecretaryService],
 })
 export class QueueModule {}
