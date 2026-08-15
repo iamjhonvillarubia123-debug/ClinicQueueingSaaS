6a851d60599b011d12ee20c1597f478679900b1a
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-15 21:22:21 +0800

    m2s2h2 wire emergency restoration service

--- src/system-admin/system-admin.module.ts
+++ src/system-admin/system-admin.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { QueueModule } from '../queue/queue.module';
+import { SystemAdminEmergencyRestoreService } from './system-admin-emergency-restore.service';
 import { SystemAdminEmergencyService } from './system-admin-emergency.service';
 import { SystemAdminController } from './system-admin.controller';
 import { SystemAdminService } from './system-admin.service';
 
 @Module({
   imports: [PrismaModule, AuthModule, QueueModule],
-  providers: [SystemAdminService, SystemAdminEmergencyService],
+  providers: [
+    SystemAdminService,
+    SystemAdminEmergencyService,
+    SystemAdminEmergencyRestoreService,
+  ],
   controllers: [SystemAdminController],
 })
 export class SystemAdminModule {}
