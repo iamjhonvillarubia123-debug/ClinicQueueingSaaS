5da8c0177083d383dc6ece02dc1a15a9f5074f02
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-15 23:50:07 +0800

    m3s8c wire activation service

--- src/practice-location/practice-location.module.ts
+++ src/practice-location/practice-location.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ScheduleModule } from '../schedule/schedule.module';
+import { PracticeLocationActivationService } from './practice-location-activation.service';
 import { PracticeLocationLifecycleService } from './practice-location-lifecycle.service';
 import { PracticeLocationPermanentDeleteService } from './practice-location-permanent-delete.service';
 import { PracticeLocationService } from './practice-location.service';
 import { PracticeLocationController } from './practice-location.controller';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, ScheduleModule],
   providers: [
     PracticeLocationService,
+    PracticeLocationActivationService,
     PracticeLocationLifecycleService,
     PracticeLocationPermanentDeleteService,
   ],
