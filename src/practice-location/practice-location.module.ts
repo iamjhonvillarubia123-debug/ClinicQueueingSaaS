f9bf455b73d566acd2b39628deca7dc1f039b10e
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-15 23:05:49 +0800

    m3s6 wire practice location permanent delete service

--- src/practice-location/practice-location.module.ts
+++ src/practice-location/practice-location.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PracticeLocationLifecycleService } from './practice-location-lifecycle.service';
+import { PracticeLocationPermanentDeleteService } from './practice-location-permanent-delete.service';
 import { PracticeLocationService } from './practice-location.service';
 import { PracticeLocationController } from './practice-location.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule],
-  providers: [PracticeLocationService, PracticeLocationLifecycleService],
+  providers: [
+    PracticeLocationService,
+    PracticeLocationLifecycleService,
+    PracticeLocationPermanentDeleteService,
+  ],
   controllers: [PracticeLocationController],
 })
 export class PracticeLocationModule {}
