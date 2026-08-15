17787126efab4c88dad3e565e6ed97c4ec6c7750
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-16 00:01:18 +0800

    m3s9a wire secretary schedule proposal service

--- src/secretary-settings-draft/secretary-settings-draft.module.ts
+++ src/secretary-settings-draft/secretary-settings-draft.module.ts
@@ -3,11 +3,15 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SecretarySettingsDraftController } from './secretary-settings-draft.controller';
+import { SecretarySettingsDraftScheduleService } from './secretary-settings-draft-schedule.service';
 import { SecretarySettingsDraftService } from './secretary-settings-draft.service';
 
 @Module({
   imports: [PrismaModule, AuthModule],
   controllers: [SecretarySettingsDraftController],
-  providers: [SecretarySettingsDraftService],
+  providers: [
+    SecretarySettingsDraftService,
+    SecretarySettingsDraftScheduleService,
+  ],
 })
 export class SecretarySettingsDraftModule {}
