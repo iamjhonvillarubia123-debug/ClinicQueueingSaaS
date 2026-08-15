a2d9718ab7fc33e6d32c9e3973d38b55e7269db6
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-15 21:12:40 +0800

    m2s2h1 wire emergency suspension service

--- src/system-admin/system-admin.module.ts
+++ src/system-admin/system-admin.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { QueueModule } from '../queue/queue.module';
+import { SystemAdminEmergencyService } from './system-admin-emergency.service';
 import { SystemAdminController } from './system-admin.controller';
 import { SystemAdminService } from './system-admin.service';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
-  providers: [SystemAdminService],
+  imports: [PrismaModule, AuthModule, QueueModule],
+  providers: [SystemAdminService, SystemAdminEmergencyService],
   controllers: [SystemAdminController],
 })
 export class SystemAdminModule {}
