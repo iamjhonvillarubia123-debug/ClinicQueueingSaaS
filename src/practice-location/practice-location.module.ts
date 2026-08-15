25b6e6c103f268507c44547f6ca24f70fb4f9578
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-15 22:51:12 +0800

    m3s5 register practice location lifecycle service

--- src/practice-location/practice-location.module.ts
+++ src/practice-location/practice-location.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PracticeLocationLifecycleService } from './practice-location-lifecycle.service';
 import { PracticeLocationService } from './practice-location.service';
 import { PracticeLocationController } from './practice-location.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule],
-  providers: [PracticeLocationService],
+  providers: [PracticeLocationService, PracticeLocationLifecycleService],
   controllers: [PracticeLocationController],
 })
 export class PracticeLocationModule {}
