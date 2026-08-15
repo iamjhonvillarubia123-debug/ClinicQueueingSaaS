f9e850e318085a69139647d75f03f8dfed0b678d
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-15 22:19:19 +0800

    m3s3 wire settings draft module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { PracticeStaffModule } from './practice-staff/practice-staff.module';
 import { PatientModule } from './patient/patient.module';
 import { BookingModule } from './booking/booking.module';
 import { SecretaryModule } from './secretary/secretary.module';
+import { SecretarySettingsDraftModule } from './secretary-settings-draft/secretary-settings-draft.module';
 import { SystemAdminModule } from './system-admin/system-admin.module';
 
 @Module({
@@ -20,6 +21,7 @@ import { SystemAdminModule } from './system-admin/system-admin.module';
     PrismaModule,
     DoctorModule,
     SecretaryModule,
+    SecretarySettingsDraftModule,
     SystemAdminModule,
     PracticeLocationModule,
     PracticeStaffModule,
