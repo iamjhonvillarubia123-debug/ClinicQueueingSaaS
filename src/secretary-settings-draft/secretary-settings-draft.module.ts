6bca7aea31d1fa66d3da94782d74e75dde7a5bd1
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-16 12:07:25 +0800

    m3s9b wire exception proposal service

--- src/secretary-settings-draft/secretary-settings-draft.module.ts
+++ src/secretary-settings-draft/secretary-settings-draft.module.ts
@@ -2,16 +2,19 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ScheduleModule } from '../schedule/schedule.module';
 import { SecretarySettingsDraftController } from './secretary-settings-draft.controller';
+import { SecretarySettingsDraftExceptionService } from './secretary-settings-draft-exception.service';
 import { SecretarySettingsDraftScheduleService } from './secretary-settings-draft-schedule.service';
 import { SecretarySettingsDraftService } from './secretary-settings-draft.service';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, ScheduleModule],
   controllers: [SecretarySettingsDraftController],
   providers: [
     SecretarySettingsDraftService,
     SecretarySettingsDraftScheduleService,
+    SecretarySettingsDraftExceptionService,
   ],
 })
 export class SecretarySettingsDraftModule {}
