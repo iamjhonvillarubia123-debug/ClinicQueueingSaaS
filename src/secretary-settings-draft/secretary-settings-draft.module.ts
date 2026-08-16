6de77b4b7d511a0f04b5bd6b6e89e4c8348fbe18
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-16 12:44:33 +0800

    m3s9d wire booking question proposal service

--- src/secretary-settings-draft/secretary-settings-draft.module.ts
+++ src/secretary-settings-draft/secretary-settings-draft.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ScheduleModule } from '../schedule/schedule.module';
+import { SecretarySettingsDraftBookingQuestionService } from './secretary-settings-draft-booking-question.service';
 import { SecretarySettingsDraftController } from './secretary-settings-draft.controller';
 import { SecretarySettingsDraftExceptionService } from './secretary-settings-draft-exception.service';
 import { SecretarySettingsDraftScheduleService } from './secretary-settings-draft-schedule.service';
@@ -17,6 +18,7 @@ import { SecretarySettingsDraftService } from './secretary-settings-draft.servic
     SecretarySettingsDraftScheduleService,
     SecretarySettingsDraftExceptionService,
     SecretarySettingsDraftServiceProposalService,
+    SecretarySettingsDraftBookingQuestionService,
   ],
 })
 export class SecretarySettingsDraftModule {}
