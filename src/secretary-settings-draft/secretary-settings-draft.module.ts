b1877aaddb25c7633cdd8e89803b856751f5d337
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-16 13:02:32 +0800

    m3s10 register settings draft approval service

--- src/secretary-settings-draft/secretary-settings-draft.module.ts
+++ src/secretary-settings-draft/secretary-settings-draft.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ScheduleModule } from '../schedule/schedule.module';
+import { SecretarySettingsDraftApprovalService } from './secretary-settings-draft-approval.service';
 import { SecretarySettingsDraftBookingQuestionService } from './secretary-settings-draft-booking-question.service';
 import { SecretarySettingsDraftController } from './secretary-settings-draft.controller';
 import { SecretarySettingsDraftExceptionService } from './secretary-settings-draft-exception.service';
@@ -19,6 +20,7 @@ import { SecretarySettingsDraftService } from './secretary-settings-draft.servic
     SecretarySettingsDraftExceptionService,
     SecretarySettingsDraftServiceProposalService,
     SecretarySettingsDraftBookingQuestionService,
+    SecretarySettingsDraftApprovalService,
   ],
 })
 export class SecretarySettingsDraftModule {}
