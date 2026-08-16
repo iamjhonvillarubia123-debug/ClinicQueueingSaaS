74ab4715b23db27efba50c67b1ebc3bc36cf20a6
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-17 00:52:13 +0800

    m6s1 export queue number allocator

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { NotificationModule } from '../notification/notification.module';
 import { ClinicDayCancellationService } from './clinic-day-cancellation.service';
+import { QueueNumberAllocationService } from './queue-number-allocation.service';
 import { SubstituteSecretaryController } from './substitute-secretary.controller';
 import { SubstituteSecretaryService } from './substitute-secretary.service';
 
 @Module({
   imports: [AuthModule, NotificationModule],
-  providers: [ClinicDayCancellationService, SubstituteSecretaryService],
+  providers: [
+    ClinicDayCancellationService,
+    QueueNumberAllocationService,
+    SubstituteSecretaryService,
+  ],
   controllers: [SubstituteSecretaryController],
-  exports: [ClinicDayCancellationService, SubstituteSecretaryService],
+  exports: [
+    ClinicDayCancellationService,
+    QueueNumberAllocationService,
+    SubstituteSecretaryService,
+  ],
 })
 export class QueueModule {}
