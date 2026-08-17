a668fb26ef4862b460329da854d2608038cd3804
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-17 12:45:54 +0800

    m6s3 add booking group outbox relation migration

---
+++ prisma/migrations/20260817050000_m6s3_booking_group_notification_outbox/migration.sql
@@ -0,0 +1,11 @@
+-- M6S3: propagate the approved BookingGroup correlation onto NotificationOutbox.
+ALTER TABLE "NotificationOutbox"
+ADD COLUMN "bookingGroupId" UUID;
+
+CREATE INDEX "NotificationOutbox_bookingGroup_idx"
+ON "NotificationOutbox"("bookingGroupId");
+
+ALTER TABLE "NotificationOutbox"
+ADD CONSTRAINT "NotificationOutbox_bookingGroupId_fkey"
+FOREIGN KEY ("bookingGroupId") REFERENCES "BookingGroup"("id")
+ON DELETE RESTRICT ON UPDATE CASCADE;
