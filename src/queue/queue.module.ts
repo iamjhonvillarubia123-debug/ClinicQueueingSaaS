89830b3a319f25d1c59ba467f909c84e969b6f37
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-17 14:28:46 +0800

    m7s1 wire start clinic service

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,22 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { IdempotencyModule } from '../idempotency/idempotency.module';
 import { NotificationModule } from '../notification/notification.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { ScheduleModule } from '../schedule/schedule.module';
 import { ClinicDayCancellationService } from './clinic-day-cancellation.service';
 import { QueueNumberAllocationService } from './queue-number-allocation.service';
+import { StartClinicController } from './start-clinic.controller';
+import { StartClinicService } from './start-clinic.service';
 import { SubstituteSecretaryController } from './substitute-secretary.controller';
 import { SubstituteSecretaryService } from './substitute-secretary.service';
 
 @Module({
-  imports: [AuthModule, NotificationModule],
+  imports: [
+    AuthModule,
+    IdempotencyModule,
+    NotificationModule,
+    PrismaModule,
+    ScheduleModule,
+  ],
   providers: [
     ClinicDayCancellationService,
     QueueNumberAllocationService,
+    StartClinicService,
     SubstituteSecretaryService,
   ],
-  controllers: [SubstituteSecretaryController],
+  controllers: [StartClinicController, SubstituteSecretaryController],
   exports: [
     ClinicDayCancellationService,
     QueueNumberAllocationService,
+    StartClinicService,
     SubstituteSecretaryService,
   ],
 })
