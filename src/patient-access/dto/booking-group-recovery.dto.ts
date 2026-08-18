35bd221b0ec8b0007c12fa5cab901b15df328924
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-18 13:29:17 +0800

    m8s2 add booking group recovery dto

---
+++ src/patient-access/dto/booking-group-recovery.dto.ts
@@ -0,0 +1,22 @@
+import { IsDateString, IsNotEmpty, IsString, IsUUID, Length } from 'class-validator';
+
+export class RequestBookingGroupRecoveryDto {
+  @IsUUID()
+  practiceLocationId!: string;
+
+  @IsDateString()
+  serviceDate!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  mobileNumber!: string;
+}
+
+export class VerifyBookingGroupRecoveryOtpDto {
+  @IsUUID()
+  recoveryAttemptId!: string;
+
+  @IsString()
+  @Length(6, 6)
+  otp!: string;
+}
