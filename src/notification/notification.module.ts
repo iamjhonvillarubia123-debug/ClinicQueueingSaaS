42a6f1ce615d54ff6306a66837f6760160b2ec0d
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-18 19:03:56 +0800

    wire m9s1 notification outbox claim service

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { NotificationOutboxClaimService } from './notification-outbox-claim.service';
 import { NotificationPayloadService } from './notification-payload.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [NotificationPayloadService],
-  exports: [NotificationPayloadService],
+  providers: [NotificationPayloadService, NotificationOutboxClaimService],
+  exports: [NotificationPayloadService, NotificationOutboxClaimService],
 })
 export class NotificationModule {}
