af7bd877ced27a9d46816122aca25a08695b1c7e
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-18 20:11:30 +0800

    m9s2 register delivery attempt service

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { NotificationDeliveryAttemptService } from './notification-delivery-attempt.service';
 import { NotificationOutboxClaimService } from './notification-outbox-claim.service';
 import { NotificationPayloadService } from './notification-payload.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [NotificationPayloadService, NotificationOutboxClaimService],
-  exports: [NotificationPayloadService, NotificationOutboxClaimService],
+  providers: [
+    NotificationPayloadService,
+    NotificationOutboxClaimService,
+    NotificationDeliveryAttemptService,
+  ],
+  exports: [
+    NotificationPayloadService,
+    NotificationOutboxClaimService,
+    NotificationDeliveryAttemptService,
+  ],
 })
 export class NotificationModule {}
