d00f9655e4b8f3c555bc18cfa4cc9337f41c9ef6
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-18 21:37:31 +0800

    m9s4 register delivery worker orchestration

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,23 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MobileNumberModule } from '../security/mobile-number/mobile-number.module';
 import { NotificationDeliveryAttemptService } from './notification-delivery-attempt.service';
+import { NotificationDeliveryWorkerService } from './notification-delivery-worker.service';
 import { NotificationOutboxClaimService } from './notification-outbox-claim.service';
 import { NotificationOutboxReconciliationService } from './notification-outbox-reconciliation.service';
 import { NotificationPayloadService } from './notification-payload.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, MobileNumberModule],
   providers: [
     NotificationPayloadService,
     NotificationOutboxClaimService,
     NotificationDeliveryAttemptService,
     NotificationOutboxReconciliationService,
+    NotificationDeliveryWorkerService,
   ],
   exports: [
     NotificationPayloadService,
     NotificationOutboxClaimService,
     NotificationDeliveryAttemptService,
     NotificationOutboxReconciliationService,
+    NotificationDeliveryWorkerService,
   ],
 })
 export class NotificationModule {}
