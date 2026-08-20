7101ec91f973e5bb7bdb14e6b0774c80ce345d32
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-21 02:39:08 +0800

    m11s3 expose public queue route

--- src/public-routing/public-routing.controller.ts
+++ src/public-routing/public-routing.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { PublicQueueService } from './public-queue.service';
 import { PublicRoutingService } from './public-routing.service';
 
 @Controller('public')
 export class PublicRoutingController {
-  constructor(private readonly publicRouting: PublicRoutingService) {}
+  constructor(
+    private readonly publicRouting: PublicRoutingService,
+    private readonly publicQueue: PublicQueueService,
+  ) {}
 
   @Get('doctors/:publicIdentifier')
   getDoctor(@Param('publicIdentifier') publicIdentifier: string) {
@@ -13,5 +17,13 @@ export class PublicRoutingController {
   @Get('practice-locations/:publicIdentifier')
   getPracticeLocation(@Param('publicIdentifier') publicIdentifier: string) {
     return this.publicRouting.getPracticeLocationPublicRoute(publicIdentifier);
+  }
+
+  @Get('practice-locations/:publicIdentifier/queue/:serviceDate')
+  getPublicQueue(
+    @Param('publicIdentifier') publicIdentifier: string,
+    @Param('serviceDate') serviceDate: string,
+  ) {
+    return this.publicQueue.getPublicQueue(publicIdentifier, serviceDate);
   }
 }
