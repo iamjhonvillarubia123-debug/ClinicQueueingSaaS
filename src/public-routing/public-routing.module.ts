ff8d648bdf783e45fc84b24d8c7fc4352060e0c5
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-21 02:39:15 +0800

    m11s3 register public queue projection

--- src/public-routing/public-routing.module.ts
+++ src/public-routing/public-routing.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FinancialModule } from '../financial/financial.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PublicQueueService } from './public-queue.service';
 import { PublicRoutingController } from './public-routing.controller';
 import { PublicRoutingService } from './public-routing.service';
 
 @Module({
   imports: [PrismaModule, FinancialModule],
-  providers: [PublicRoutingService],
+  providers: [PublicRoutingService, PublicQueueService],
   controllers: [PublicRoutingController],
 })
 export class PublicRoutingModule {}
