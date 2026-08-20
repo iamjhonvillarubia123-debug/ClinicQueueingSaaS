dd6f021ea75c799eab2abeb4ba17618b38f93eba
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-20 21:23:30 +0800

    m10 wire subscription period and quote services

--- src/financial/financial.module.ts
+++ src/financial/financial.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { FinancialAccountLockService } from './financial-account-lock.service';
 import { SubscriptionEntitlementService } from './subscription-entitlement.service';
+import { SubscriptionPeriodService } from './subscription-period.service';
+import { SubscriptionPurchaseQuoteService } from './subscription-purchase-quote.service';
 
 @Module({
-  providers: [FinancialAccountLockService, SubscriptionEntitlementService],
-  exports: [FinancialAccountLockService, SubscriptionEntitlementService],
+  providers: [
+    FinancialAccountLockService,
+    SubscriptionEntitlementService,
+    SubscriptionPeriodService,
+    SubscriptionPurchaseQuoteService,
+  ],
+  exports: [
+    FinancialAccountLockService,
+    SubscriptionEntitlementService,
+    SubscriptionPeriodService,
+    SubscriptionPurchaseQuoteService,
+  ],
 })
 export class FinancialModule {}
