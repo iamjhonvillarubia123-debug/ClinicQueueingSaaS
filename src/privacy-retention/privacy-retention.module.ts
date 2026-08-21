189fddfb804c915eb613961bd078d7ae56ed86b7
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-21 18:47:34 +0800

    m12s4 register security retention cleanup service

--- src/privacy-retention/privacy-retention.module.ts
+++ src/privacy-retention/privacy-retention.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AppointmentErasureService } from './appointment-erasure.service';
 import { PrivacyRetentionService } from './privacy-retention.service';
+import { SecurityRetentionCleanupService } from './security-retention-cleanup.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [PrivacyRetentionService, AppointmentErasureService],
-  exports: [PrivacyRetentionService, AppointmentErasureService],
+  providers: [
+    PrivacyRetentionService,
+    AppointmentErasureService,
+    SecurityRetentionCleanupService,
+  ],
+  exports: [
+    PrivacyRetentionService,
+    AppointmentErasureService,
+    SecurityRetentionCleanupService,
+  ],
 })
 export class PrivacyRetentionModule {}
