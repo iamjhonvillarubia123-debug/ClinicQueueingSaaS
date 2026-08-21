8f6f15d066a9709feb6ed18c7835e8a133375b34
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-21 14:54:32 +0800

    m12s4 register command idempotency cleanup

--- src/idempotency/idempotency.module.ts
+++ src/idempotency/idempotency.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CommandIdempotencyCleanupService } from './command-idempotency-cleanup.service';
 import { CommandIdempotencyService } from './command-idempotency.service';
 
 @Module({
-  providers: [CommandIdempotencyService],
-  exports: [CommandIdempotencyService],
+  providers: [CommandIdempotencyService, CommandIdempotencyCleanupService],
+  exports: [CommandIdempotencyService, CommandIdempotencyCleanupService],
 })
 export class IdempotencyModule {}
