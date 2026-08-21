727698ad1569547659adb61eef75f23269471243
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-21 22:54:33 +0800

    m13s1 fix main formatting

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,10 @@ async function bootstrap() {
     response.setHeader('X-Content-Type-Options', 'nosniff');
     response.setHeader('X-Frame-Options', 'DENY');
     response.setHeader('Referrer-Policy', 'no-referrer');
-    response.setHeader('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+    response.setHeader(
+      'Permissions-Policy',
+      'camera=(), microphone=(), geolocation=()',
+    );
     response.setHeader('Cross-Origin-Resource-Policy', 'same-site');
     if (isProduction) {
       response.setHeader(
