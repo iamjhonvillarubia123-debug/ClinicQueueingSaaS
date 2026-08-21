69a800d6a0b24041668a3626aa5aac8a22dfa0a9
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-21 13:34:11 +0800

    m12s2 register appointment erasure service

--- src/privacy-retention/privacy-retention.module.ts
+++ src/privacy-retention/privacy-retention.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AppointmentErasureService } from './appointment-erasure.service';
 import { PrivacyRetentionService } from './privacy-retention.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [PrivacyRetentionService],
-  exports: [PrivacyRetentionService],
+  providers: [PrivacyRetentionService, AppointmentErasureService],
+  exports: [PrivacyRetentionService, AppointmentErasureService],
 })
 export class PrivacyRetentionModule {}
