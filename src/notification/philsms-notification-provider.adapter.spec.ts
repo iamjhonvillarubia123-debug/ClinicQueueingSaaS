75abd129ac6dd150745a78f1e8b9b660256fbd30
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-22 00:52:07 +0800

    m13s3 fix philsms fetch mock signature

--- src/notification/philsms-notification-provider.adapter.spec.ts
+++ src/notification/philsms-notification-provider.adapter.spec.ts
@@ -50,14 +50,16 @@ describe('PhilSmsNotificationProviderAdapter', () => {
   });
 
   it('captures PhilSMS UID and accepted status on successful submission', async () => {
-    const fetchMock = jest.fn<typeof fetch>(
-      (_input: Parameters<typeof fetch>[0], _init?: RequestInit) =>
-        Promise.resolve(
-          response(200, {
-            status: 'success',
-            data: { uid: 'sms-uid-1', status: 'queued' },
-          }),
-        ),
+    const fetchMock = jest.fn<
+      Promise<Response>,
+      [RequestInfo | URL, RequestInit?]
+    >((_input, _init) =>
+      Promise.resolve(
+        response(200, {
+          status: 'success',
+          data: { uid: 'sms-uid-1', status: 'queued' },
+        }),
+      ),
     );
     global.fetch = fetchMock;
 
