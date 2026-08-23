7d222b2f07fa6b9bca7be51a07349083445cee39
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-23 20:22:41 +0800

    fix secretary registration dependency wiring

--- src/secretary/secretary.module.ts
+++ src/secretary/secretary.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { MobileNumberModule } from '../security/mobile-number/mobile-number.module';
 import { SecretaryLifecycleService } from './secretary-lifecycle.service';
 import { SecretaryRegistrationService } from './secretary-registration.service';
 import { SecretaryController } from './secretary.controller';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, MobileNumberModule],
   providers: [SecretaryLifecycleService, SecretaryRegistrationService],
   controllers: [SecretaryController],
 })
