e3c26a83278e7365ff105faa4486820ad629a166
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-23 20:41:51 +0800

    fix secretary workspace auth module dependency

--- src/secretary-workspace/secretary-workspace.module.ts
+++ src/secretary-workspace/secretary-workspace.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SecretaryWorkspaceController } from './secretary-workspace.controller';
 import { SecretaryWorkspaceService } from './secretary-workspace.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [SecretaryWorkspaceController],
   providers: [SecretaryWorkspaceService],
 })
