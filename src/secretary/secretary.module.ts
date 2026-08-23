29866828e29e35af2324eae4cc4df62781c6e590
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-23 19:28:10 +0800

    wire secretary registration service

--- src/secretary/secretary.module.ts
+++ src/secretary/secretary.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SecretaryLifecycleService } from './secretary-lifecycle.service';
+import { SecretaryRegistrationService } from './secretary-registration.service';
 import { SecretaryController } from './secretary.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule],
-  providers: [SecretaryLifecycleService],
+  providers: [SecretaryLifecycleService, SecretaryRegistrationService],
   controllers: [SecretaryController],
 })
 export class SecretaryModule {}
