79842b999bfc175e33d314b469c68ddf0962c1f9
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-24 15:28:47 +0800

    f7 add replacement invitation dto

---
+++ src/secretary/dto/create-secretary-replacement-invitation.dto.ts
@@ -0,0 +1,70 @@
+import { Transform } from 'class-transformer';
+import {
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+} from 'class-validator';
+import { SecretaryAccessProfile } from '../../../generated/prisma/client';
+
+const trimString = ({ value }: { value: unknown }): unknown =>
+  typeof value === 'string' ? value.trim() : value;
+
+export class CreateSecretaryReplacementInvitationDto {
+  @IsUUID()
+  practiceLocationId!: string;
+
+  @Transform(trimString)
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  firstName!: string;
+
+  @Transform(trimString)
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  lastName!: string;
+
+  @Transform(trimString)
+  @IsEmail()
+  @MaxLength(255)
+  email!: string;
+
+  @Transform(trimString)
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
+  mobileNumber!: string;
+
+  @IsEnum(SecretaryAccessProfile)
+  accessProfile!: SecretaryAccessProfile;
+
+  @IsOptional()
+  @IsBoolean()
+  canManageClinicDetails?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  canManageServices?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  canManageBookingQuestions?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  canManageSchedules?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  cancelClinicDay?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  assignDaySecretary?: boolean;
+}
