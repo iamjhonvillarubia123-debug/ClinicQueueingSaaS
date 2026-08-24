b5e3fff3449ccf46ae7ea696c22de58f03977b4e
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-24 15:29:59 +0800

    f7 register replacement invitation service

--- src/secretary/secretary.module.ts
+++ src/secretary/secretary.module.ts
@@ -4,11 +4,17 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { MobileNumberModule } from '../security/mobile-number/mobile-number.module';
 import { SecretaryInvitationService } from './secretary-invitation.service';
 import { SecretaryLifecycleService } from './secretary-lifecycle.service';
+import { SecretaryReplacementInvitationController } from './secretary-replacement-invitation.controller';
+import { SecretaryReplacementInvitationService } from './secretary-replacement-invitation.service';
 import { SecretaryController } from './secretary.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule, MobileNumberModule],
-  providers: [SecretaryLifecycleService, SecretaryInvitationService],
-  controllers: [SecretaryController],
+  providers: [
+    SecretaryLifecycleService,
+    SecretaryInvitationService,
+    SecretaryReplacementInvitationService,
+  ],
+  controllers: [SecretaryController, SecretaryReplacementInvitationController],
 })
 export class SecretaryModule {}
