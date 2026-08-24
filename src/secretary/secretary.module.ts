8e163ff6247a7897af08c6c746fe2e633e6f67a8
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-24 12:13:48 +0800

    f7 wire secretary invitation service

--- src/secretary/secretary.module.ts
+++ src/secretary/secretary.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { MobileNumberModule } from '../security/mobile-number/mobile-number.module';
+import { SecretaryInvitationService } from './secretary-invitation.service';
 import { SecretaryLifecycleService } from './secretary-lifecycle.service';
-import { SecretaryRegistrationService } from './secretary-registration.service';
 import { SecretaryController } from './secretary.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule, MobileNumberModule],
-  providers: [SecretaryLifecycleService, SecretaryRegistrationService],
+  providers: [SecretaryLifecycleService, SecretaryInvitationService],
   controllers: [SecretaryController],
 })
 export class SecretaryModule {}
