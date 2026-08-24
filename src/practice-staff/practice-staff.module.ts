8030957410ca94b03f0b18d58d25a41990903f1f
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-24 15:04:47 +0800

    register secretary access service

--- src/practice-staff/practice-staff.module.ts
+++ src/practice-staff/practice-staff.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 
+import { PracticeStaffAccessService } from './practice-staff-access.service';
 import { PracticeStaffReadService } from './practice-staff-read.service';
 import { PracticeStaffService } from './practice-staff.service';
 import { PracticeStaffController } from './practice-staff.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule],
-  providers: [PracticeStaffService, PracticeStaffReadService],
+  providers: [PracticeStaffService, PracticeStaffReadService, PracticeStaffAccessService],
   controllers: [PracticeStaffController],
 })
 export class PracticeStaffModule {}
