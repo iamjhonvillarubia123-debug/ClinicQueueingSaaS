4e1c40946d8d2365a71429d5d4739639056ba3e6
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-28 23:26:21 +0800

    feat(clinics): add protected apply settings command dto

---
+++ src/practice-location/dto/apply-practice-location-configuration-draft.dto.ts
@@ -0,0 +1,15 @@
+import { Equals, IsBoolean, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class ApplyPracticeLocationConfigurationDraftDto {
+  @IsUUID()
+  @IsNotEmpty()
+  practiceLocationId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password!: string;
+
+  @IsBoolean()
+  @Equals(true)
+  confirmApply!: boolean;
+}
