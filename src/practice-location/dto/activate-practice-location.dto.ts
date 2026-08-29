b7342116ccccb39aac7808703f272bb8a7bf53ca
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-29 12:42:23 +0800

    feat(clinics): protect clinic activation

--- src/practice-location/dto/activate-practice-location.dto.ts
+++ src/practice-location/dto/activate-practice-location.dto.ts
@@ -1,6 +1,13 @@
-import { IsUUID } from 'class-validator';
+import { IsBoolean, IsString, IsUUID, MinLength } from 'class-validator';
 
 export class ActivatePracticeLocationDto {
   @IsUUID()
   practiceLocationId!: string;
+
+  @IsString()
+  @MinLength(1)
+  password!: string;
+
+  @IsBoolean()
+  confirmActivation!: boolean;
 }
