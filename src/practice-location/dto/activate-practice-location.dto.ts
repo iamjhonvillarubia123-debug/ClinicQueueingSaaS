d6402f6a3ab576be09c97f7100a9b62c324d099f
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-29 12:45:34 +0800

    fix(clinics): type protected activation fields

--- src/practice-location/dto/activate-practice-location.dto.ts
+++ src/practice-location/dto/activate-practice-location.dto.ts
@@ -1,21 +1,13 @@
-import {
-  IsBoolean,
-  IsDefined,
-  IsString,
-  IsUUID,
-  MinLength,
-} from 'class-validator';
+import { IsBoolean, IsString, IsUUID, MinLength } from 'class-validator';
 
 export class ActivatePracticeLocationDto {
   @IsUUID()
   practiceLocationId!: string;
 
-  @IsDefined()
   @IsString()
   @MinLength(1)
-  password?: string;
+  password!: string;
 
-  @IsDefined()
   @IsBoolean()
-  confirmActivation?: boolean;
+  confirmActivation!: boolean;
 }
