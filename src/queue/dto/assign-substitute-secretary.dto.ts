8c3c9e27e0bdc7c5da00c5ee498cfcad3001cafe
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-30 21:00:57 +0800

    support lazy clinic day operating assignment

--- src/queue/dto/assign-substitute-secretary.dto.ts
+++ src/queue/dto/assign-substitute-secretary.dto.ts
@@ -1,8 +1,17 @@
-import { IsUUID } from 'class-validator';
+import { IsOptional, IsUUID, Matches } from 'class-validator';
 
 export class AssignSubstituteSecretaryDto {
+  @IsOptional()
   @IsUUID()
-  clinicDayId!: string;
+  clinicDayId?: string;
+
+  @IsOptional()
+  @IsUUID()
+  practiceLocationId?: string;
+
+  @IsOptional()
+  @Matches(/^\d{4}-\d{2}-\d{2}$/)
+  serviceDate?: string;
 
   @IsUUID()
   userId!: string;
