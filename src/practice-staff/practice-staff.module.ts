8b2caf6ddf1879a806b6f1fa5dde3777c25d14af
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-30 23:38:44 +0800

    register f5 clinic secretary authority service

--- src/practice-staff/practice-staff.module.ts
+++ src/practice-staff/practice-staff.module.ts
@@ -3,12 +3,13 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 
+import { ClinicSecretaryAuthorityService } from './clinic-secretary-authority.service';
 import { PracticeStaffService } from './practice-staff.service';
 import { PracticeStaffController } from './practice-staff.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule],
-  providers: [PracticeStaffService],
+  providers: [PracticeStaffService, ClinicSecretaryAuthorityService],
   controllers: [PracticeStaffController],
 })
 export class PracticeStaffModule {}
