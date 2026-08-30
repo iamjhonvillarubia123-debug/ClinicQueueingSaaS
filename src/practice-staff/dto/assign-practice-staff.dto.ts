76e8a86ba0e2408ee16b2c05a213f6be0215ac2d
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-08-30 23:32:13 +0800

    require authority bundles for clinic secretary assignment

--- src/practice-staff/dto/assign-practice-staff.dto.ts
+++ src/practice-staff/dto/assign-practice-staff.dto.ts
@@ -1,4 +1,13 @@
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import {
+  ArrayNotEmpty,
+  ArrayUnique,
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+  IsUUID,
+} from 'class-validator';
+
+import { ClinicSecretaryAuthorityBundle } from '../secretary-authority.types';
 
 export class AssignPracticeStaffDto {
   @IsUUID()
@@ -8,4 +17,10 @@ export class AssignPracticeStaffDto {
   @IsUUID()
   @IsNotEmpty()
   userId!: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @ArrayUnique()
+  @IsEnum(ClinicSecretaryAuthorityBundle, { each: true })
+  authorityBundles!: ClinicSecretaryAuthorityBundle[];
 }
