2bac412fe5e584af237e8074bd857b9a28856cfa
iamjhonvillarubia123-debug <iamjhonvillarubia123@gmail.com>
2026-09-06 12:38:14 +0800

    feat(r3): identify secretary invitations by email or mobile

--- src/practice-staff/dto/create-secretary-invitation.dto.ts
+++ src/practice-staff/dto/create-secretary-invitation.dto.ts
@@ -3,7 +3,6 @@ import {
   ArrayUnique,
   IsBoolean,
   IsArray,
-  IsEmail,
   IsEnum,
   IsNotEmpty,
   IsOptional,
@@ -23,10 +22,11 @@ export enum SecretaryInvitationAssignmentType {
 
 export class CreateSecretaryInvitationDto {
   @IsUUID() @IsNotEmpty() practiceLocationId!: string;
-  @IsString() @IsNotEmpty() @MaxLength(100) firstName!: string;
-  @IsString() @IsNotEmpty() @MaxLength(100) lastName!: string;
-  @IsEmail() @MaxLength(255) email!: string;
-  @IsString() @IsNotEmpty() @MaxLength(30) mobileNumber!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  identifier!: string;
 
   @IsEnum(SecretaryInvitationAssignmentType)
   assignmentType!: SecretaryInvitationAssignmentType;
